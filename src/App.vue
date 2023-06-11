2d98bb9220bc437c0bd0e519ba95c54c5a43bee1
siboniseni <siboniseni.kasa@outlook.com>
2023-06-11 13:48:33 +0200

    html structure started

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="addNewProject">New Project</button>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      projects: [
+        { id: 1, name: 'Project 1' },
+        { id: 2, name: 'Project 2' },
+        { id: 3, name: 'Project 3' },
+      ],
+      projectIdCounter: 4,
+    };
+  },
+  methods: {
+    addNewProject() {
+      const newProject = {
+        id: this.projectIdCounter,
+        name: 'New Project ' + this.projectIdCounter,
+      };
+      this.projects.unshift(newProject);
+      this.projectIdCounter++;
+    },
+  },
+});
+</script>
