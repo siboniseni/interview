728ccc1129c0a118f21f9bcca0a6d2656015143b
siboniseni <siboniseni.kasa@outlook.com>
2023-06-11 14:08:30 +0200

    App.vue modified

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <button class="new-project-button" @click="addNewProject">New Project</button>
+    <button class="new-project-button" @click="showForm = true">New Project</button>
     <ul class="project-list">
       <li v-for="project in projects" :key="project.id" class="project-item">
         <div class="project-details">
@@ -10,6 +10,22 @@
         </div>
       </li>
     </ul>
+
+    <div class="form-modal" v-if="showForm">
+      <div class="form-modal-content">
+        <h2>Create New Project</h2>
+        <form @submit.prevent="createProject">
+          <label for="customerName">Customer Name:</label>
+          <input type="text" id="customerName" v-model="newProject.customerName" required>
+
+          <label for="projectName">Project Name:</label>
+          <input type="text" id="projectName" v-model="newProject.projectName" required>
+
+          <button type="submit">Create</button>
+        </form>
+        <button class="close-button" @click="showForm = false">Close</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,18 +42,26 @@ export default defineComponent({
         { id: 3, customerName: 'Alex Johnson', projectName: 'Project 3', createdAt: new Date() },
       ],
       projectIdCounter: 4,
+      newProject: {
+        customerName: '',
+        projectName: '',
+      },
+      showForm: false,
     };
   },
   methods: {
-    addNewProject() {
+    createProject() {
       const newProject = {
         id: this.projectIdCounter,
-        customerName: 'New Customer',
-        projectName: 'New Project ' + this.projectIdCounter,
+        customerName: this.newProject.customerName,
+        projectName: this.newProject.projectName,
         createdAt: new Date(),
       };
       this.projects.unshift(newProject);
       this.projectIdCounter++;
+      this.newProject.customerName = '';
+      this.newProject.projectName = '';
+      this.showForm = false;
     },
     calculateTimeAgo(createdAt: Date) {
       return moment(createdAt).fromNow();
@@ -91,4 +115,64 @@ export default defineComponent({
   color: gray;
   font-size: 12px;
 }
+
+.form-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.form-modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.form-modal-content h2 {
+  margin-top: 0;
+}
+
+.form-modal-content form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-modal-content label {
+  margin-top: 10px;
+}
+
+.form-modal-content input {
+  padding: 5px;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+  margin-top: 5px;
+}
+
+.form-modal-content button[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.close-button {
+  margin-top: 10px;
+  background-color: #ccc;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  font-size: 14px;
+  cursor: pointer;
+}
 </style>
+
